d829a53ee9ee2f68a0c7e4b8f5a3cdcf66be01f8
rprtr258 <poletansky@gmail.com>
2025-11-04 05:10:47 +0300

    remove vue framework make pure vanilla rerender second canvas only when formula changes, which significantly improved speed

--- root.js
+++ root.js
@@ -1,151 +1,215 @@
+function flatten(n) {
+  return n === null ? [] :
+    n instanceof Node ? [n] :
+    typeof n === "string" ? [n] :
+      n.flatMap(flatten);
+}
+
+function m(
+  tag,
+  props = {},
+  ...children
+) {
+  const el = document.createElement(tag);
+  for (const [key, value] of Object.entries(props)) {
+    if (key === "style") {
+      for (const [k, v] of Object.entries(value ?? {})) {
+        el.style.setProperty(k, v);
+      }
+    } else if (key.startsWith("on")) {
+      el.addEventListener(key.slice(2).toLowerCase(), value);
+    } else {
+      el.setAttribute(key, String(value));
+    }
+  }
+  for (const child of flatten(children)) {
+    if (child === null) {
+      continue;
+    }
+    el.append(
+      child instanceof Node ? child :
+      typeof child === "string" ? document.createTextNode(child) :
+        child[0]);
+  }
+  return el;
+}
+
 function remap(x, old_min, old_max, new_min, new_max) {
   return (x - old_min) / (old_max - old_min) * (new_max - new_min) + new_min;
 }
 
 const formulaPrelude = `
-  const sin = Math.sin;
-  const cos = Math.cos;
+  const {sin, cos, pow} = Math;
+  window.alert = window.close;
 `;
 
-export default {
-  data() {
-    return {
-      formula: "sin(1/x)",
-      error: null,
-      lowx: -1,
-      highx: 1,
-      zoomx: 0,
-      lowy: -1,
-      highy: 1,
-      zoomy: 0,
-      steps: 100,
-      examples: ["sin(1/x)", "sin(x)", "x*x", "1/x", "cos(x)", "cos(1/x)"],
+const examples = ["sin(1/x)", "sin(x)", "x*x", "1/x", "cos(x)", "cos(1/x)"];
+
+function changed(orig, patch) {
+  return patch !== undefined && patch !== orig;
+}
+
+function input(label, props) {
+  const el_input = m("input", {type: "range", ...props, oninput: e => props.oninput(Number(e.target.value))});
+  const el_value = m("div", {});
+
+  return {
+    root: m("p", {}, label, el_input, el_value),
+    set: (value) => {
+      el_input.value = value;
+      el_value.textContent = value;
+    },
+  };
+}
+
+/**
+ *
+ * @param {HTMLElement} root
+ * @returns {void}
+ */
+(function(root) {
+  const data = {
+    formula: "",
+    error: null,
+    lowx: -1,
+    highx: 1,
+    zoomx: 0,
+    lowy: -1,
+    highy: 1,
+    zoomy: 0,
+    steps: 100,
+  };
+  const updLowx = (lowx) => Math.min(lowx, data.highx - 1);
+  const lowxinput  = input("lowx",  {min: -100, max: 100, oninput: x => update({lowx:  updLowx(x)})});
+  const updHighx = (highx) => Math.max(highx, data.lowx + 1);
+  const highxinput = input("highx", {min: -100, max: 100, oninput: x => update({highx: updHighx(x)})});
+  const zoomxinput = input("zoomx", {min: -10,  max: 10,  oninput: x => update({zoomx: x}), step: 0.1});
+  const updLowy = (lowy) => Math.min(lowy, data.highy - 1);
+  const lowyinput  = input("lowy",  {min: -100, max: 100, oninput: x => update({lowy:  updLowy(x)})});
+  const updHighy = (highy) => Math.max(highy, data.lowy + 1);
+  const highyinput = input("highy", {min: -100, max: 100, oninput: x => update({highy: updHighy(x)})});
+  const zoomyinput = input("zoomy", {min: -10,  max: 10,  oninput: x => update({zoomy: x}), step: 0.1});
+  const refs = {
+    canvas:  m("canvas", {width: "400", height: "400"}),
+    canvas2: m("canvas", {width: "400", height: "400"}),
+    formula: m("input", {type: "textarea", oninput: (e) => update({formula: e.target.value})}),
+    errtext: m("span", {style: {color: "red"}}),
+  };
+  const ctx = refs.canvas.getContext("2d");
+  const ctx2 = refs.canvas2.getContext("2d");
+  const update = (patch) => {
+    // NOTE: error change does not trigger redraw
+    if (patch.error !== undefined && patch.error !== data.error) {
+      data.error = patch.error;
+      refs.errtext.textContent = data.error ?? "";
     }
-  },
-  methods: {
-    draw() {
-      try { // try to parse formula
-        eval(formulaPrelude + `const x = ${this.lowx};` + this.formula);
-      } catch (e) {
-        this.error = e.message;
-        return;
-      };
-      this.error = null;
 
-      this.ctx.fillStyle = "white";
-      this.ctx.fillRect(0, 0, this.$refs.canvas.width, this.$refs.canvas.height);
-      this.ctx.strokeStyle = "black";
-      this.ctx.beginPath();
-      const minx = this.lowx * Math.pow(2, this.zoomx);
-      const maxx = this.highx * Math.pow(2, this.zoomx);
-      const miny = this.lowy * Math.pow(2, this.zoomy);
-      const maxy = this.highy * Math.pow(2, this.zoomy);
-      const step = (maxx - minx) / this.steps;
-      console.log("drawing", minx, maxx, this.formula);
-      for (let x = minx; x < maxx; x += step) {
-        const y = eval(formulaPrelude + `const x = ${x};` + this.formula);
-        this.ctx.lineTo(
-          remap(x, minx, maxx, 0, this.$refs.canvas.width),
-          remap(y, miny, maxy, this.$refs.canvas.height, 0),
-        );
-      }
-      this.ctx.stroke();
+    let should_redraw2 = changed(data.formula, patch.formula);
+    let should_redraw =
+      should_redraw2 ||
+      changed(data.steps, patch.steps) ||
+      changed(data.lowx, patch.lowx) ||
+      changed(data.highx, patch.highx) ||
+      changed(data.zoomx, patch.zoomx) ||
+      changed(data.lowy, patch.lowy) ||
+      changed(data.highy, patch.highy) ||
+      changed(data.zoomy, patch.zoomy);
 
-      this.ctx2.fillStyle = "white";
-      this.ctx2.fillRect(0, 0, this.$refs.canvas.width, this.$refs.canvas.height);
-      this.ctx2.strokeStyle = "black";
-      this.ctx2.beginPath();
+    Object.assign(data, patch);
+    refs.formula.value = data.formula;
+    lowxinput.set(data.lowx);
+    highxinput.set(data.highx);
+    zoomxinput.set(data.zoomx);
+    lowyinput.set(data.lowy);
+    highyinput.set(data.highy);
+    zoomyinput.set(data.zoomy);
+    if (should_redraw) {
+      draw(should_redraw2);
+    }
+  };
+
+  const draw = (draw2) => {
+    try { // try to parse formula
+      eval(formulaPrelude + `const x = ${data.lowx};` + data.formula);
+    } catch (e) {
+      update({error: e.message});
+      return;
+    };
+    update({error: null});
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, refs.canvas.width, refs.canvas.height);
+    ctx.strokeStyle = "black";
+    ctx.beginPath();
+    const zoomx = Math.pow(2, data.zoomx);
+    const minx = data.lowx * zoomx;
+    const maxx = data.highx * zoomx;
+    const zoomy = Math.pow(2, data.zoomy);
+    const miny = data.lowy * zoomy;
+    const maxy = data.highy * zoomy;
+    const step = (maxx - minx) / data.steps;
+    console.log(`drawing in (${minx}, ${miny}) to (${maxx}, ${maxy}): ${data.formula}`);
+    for (let x = minx; x < maxx; x += step) {
+      const y = eval(formulaPrelude + `const x = ${x};` + data.formula);
+      ctx.lineTo(
+        remap(x, minx, maxx, 0, refs.canvas.width),
+        remap(y, miny, maxy, refs.canvas.height, 0),
+      );
+    }
+    ctx.stroke();
+
+    if (draw2) {
+      ctx2.fillStyle = "white";
+      ctx2.fillRect(0, 0, refs.canvas.width, refs.canvas.height);
+      ctx2.strokeStyle = "black";
+      ctx2.beginPath();
       const POINTS = 10000;
       for (let i = 0; i < POINTS; i++) {
         const x = Math.tan(remap(i, 0, POINTS, -Math.PI/2, Math.PI/2));
-        const y = eval(formulaPrelude + `const x = ${x};` + this.formula);
-        this.ctx2.lineTo(
-          remap(i, 0, POINTS, 0, this.$refs.canvas.width),
-          remap(Math.atan(y), -Math.PI/2, Math.PI/2, this.$refs.canvas.height, 0),
+        const y = eval(formulaPrelude + `const x = ${x};` + data.formula);
+        ctx2.lineTo(
+          remap(i, 0, POINTS, 0, refs.canvas.width),
+          remap(Math.atan(y), -Math.PI/2, Math.PI/2, refs.canvas.height, 0),
         );
       }
-      this.ctx2.stroke();
-    },
-    updLowx(lowx) {
-      this.lowx = lowx;
-      if (this.lowx >= this.highx) {
-        this.lowx = this.highx - 1;
-      }
-    },
-    updHighx(highx) {
-      this.highx = highx;
-      if (this.highx <= this.lowx) {
-        this.highx = this.lowx + 1;
-      }
-    },
-    updLowy(lowy) {
-      this.lowy = lowy;
-      if (this.lowy >= this.highy) {
-        this.lowy = this.highy - 1;
-      }
-    },
-    updHighy(highy) {
-      this.highy = highy;
-      if (this.highy <= this.lowy) {
-        this.highy = this.lowy + 1;
-      }
-    },
-  },
-  beforeUpdate() {
-    this.draw();
-  },
-  mounted() {
-    this.ctx = this.$refs.canvas.getContext("2d");
-    this.ctx2 = this.$refs.canvas2.getContext("2d");
-    this.draw();
-  },
-  template: /*html*/`<div style="display: grid; grid-template-columns: auto 1fr auto;">
-    <canvas ref="canvas" width="400" height="400" />
-    <div style="display: grid; grid-template-rows: repeat(1fr, 4); height: min-content;">
-      <div>
-        Examples: <button v-for="example in examples" @click="formula = example">{{example}}</button>
-      </div>
-      <div>Formula: <input type="textarea" v-model="formula"><span style="color: red">{{error ? error : ""}}</span></div>
-      <div>Points: <input type="number" min="100" step="100" v-model="steps"></div>
-      <div>
-        <p>
-          lowx
-          <input type="range" min="-100" max="100" :value="lowx" @input="event => updLowx(Number(event.target.value))">
-          {{lowx}}
-        </p>
-        <div style="width: 1em;"></div>
-        <p>
-          highx
-          <input type="range" min="-100" max="100" :value="highx" @input="event => updHighx(Number(event.target.value))">
-          {{highx}}
-        </p>
-        <div style="width: 1em;"></div>
-        <p>
-          zoomx
-          <input type="range" min="-10" max="10" step="0.1" v-model="zoomx">
-          {{zoomx}}
-        </p>
-      </div>
-      <div>
-        <p>
-          lowy
-          <input type="range" min="-100" max="100" :value="lowy" @input="event => updLowy(Number(event.target.value))">
-          {{lowy}}
-        </p>
-        <div style="width: 1em;"></div>
-        <p>
-          highy
-          <input type="range" min="-100" max="100" :value="highy" @input="event => updHighy(Number(event.target.value))">
-          {{highy}}
-        </p>
-        <div style="width: 1em;"></div>
-        <p>
-          zoomy
-          <input type="range" min="-10" max="10" step="0.1" v-model="zoomy">
-          {{zoomy}}
-        </p>
-      </div>
-    </div>
-    <canvas ref="canvas2" width="400" height="400" />
-  </div>`
-}
+      ctx2.stroke();
+    }
+  };
+  update({formula: "sin(1/x)"}); // NOTE: init
+
+  root.append(
+    m("div", {style: {display: "grid", "grid-template-columns": "auto 1fr auto"}},
+      refs.canvas,
+      m("div", {style: {display: "grid", "grid-template-rows": "repeat(1fr, 4)", height: "min-content"}},
+        m("div", {},
+          "Examples: ",
+          examples.map(example => m("button", {onclick: () => update({formula: example})}, example)),
+        ),
+        m("div", {},
+          "Formula: ",
+          refs.formula,
+          refs.errtext,
+        ),
+        m("div", {},
+          "Points: ",
+          m("input", {type: "number", min: "100", step: "100", value: data.steps, oninput: e => update({steps: e.target.value})}),
+        ),
+        m("div", {},
+          lowxinput.root,
+          m("div", {style: {width: "1em"}}),
+          highxinput.root,
+          m("div", {style: {width: "1em"}}),
+          zoomxinput.root,
+        ),
+        m("div", {},
+          lowyinput.root,
+          m("div", {style: {width: "1em"}}),
+          highyinput.root,
+          m("div", {style: {width: "1em"}}),
+          zoomyinput.root,
+        ),
+      ),
+      refs.canvas2,
+    ),
+  );
+})(document.body);
